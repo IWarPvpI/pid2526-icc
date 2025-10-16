d567a1e9d49f76226b8310f88c72776592960dc1
yassine <yassine>
2025-10-16 16:35:47 +0200

    After framework installation

---
+++ src/main/java/be/icc/pid2526/reservations_spring/ReservationsSpringApplication.java
@@ -0,0 +1,13 @@
+package be.icc.pid2526.reservations_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/icc/pid2526/reservations_spring/ReservationsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.icc.pid2526.reservations_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
