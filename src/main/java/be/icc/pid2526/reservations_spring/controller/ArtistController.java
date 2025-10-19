6972a4acfe59e5a9ea8f7ac211032404f41b496a
yassine <yassine>
2025-10-19 12:26:52 +0200

    Form update Artist

--- src/main/java/be/icc/pid2526/reservations_spring/controller/ArtistController.java
+++ src/main/java/be/icc/pid2526/reservations_spring/controller/ArtistController.java
@@ -2,12 +2,13 @@ package be.icc.pid2526.reservations_spring.controller;
 
 import be.icc.pid2526.reservations_spring.model.Artist;
 import be.icc.pid2526.reservations_spring.service.ArtistService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
@@ -36,6 +37,44 @@ public class ArtistController {
 
         return "artist/show";
     }
+
+    @GetMapping("/artists/{id}/edit")
+    public String edit(Model model, @PathVariable long id, HttpServletRequest request) {
+        Artist artist = service.getArtist(id);
+
+        model.addAttribute("artist", artist);
+
+
+        //Générer le lien retour pour l'annulation
+        String referrer = request.getHeader("Referer");
+
+        if(referrer!=null && !referrer.equals("")) {
+            model.addAttribute("back", referrer);
+        } else {
+            model.addAttribute("back", "/artists/"+artist.getId());
+        }
+
+        return "artist/edit";
+    }
+
+    @PutMapping("/artists/{id}/edit")
+    public String update(@Valid @ModelAttribute Artist artist, BindingResult bindingResult, @PathVariable long id, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "artist/edit";
+        }
+
+        Artist existing = service.getArtist(id);
+
+        if(existing==null) {
+            return "artist/index";
+        }
+
+        service.updateArtist(id, artist);
+
+        return "redirect:/artists/"+artist.getId();
+    }
+
 
 
 
--- src/main/java/be/icc/pid2526/reservations_spring/model/Artist.java
+++ src/main/java/be/icc/pid2526/reservations_spring/model/Artist.java
@@ -1,6 +1,8 @@
 package be.icc.pid2526.reservations_spring.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +14,11 @@ public class Artist {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
+    @NotBlank(message = "The firstname must not be empty.")
+    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String firstname;
+    @NotBlank(message = "The lasttname must not be empty.")
+    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String lastname;
 
     @Override
