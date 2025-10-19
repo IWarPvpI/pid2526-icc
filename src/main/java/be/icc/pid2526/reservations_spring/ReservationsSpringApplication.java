9842112b52dcdd03eca5d7b7210c560e4ab01224
yassine <yassine>
2025-10-19 07:54:44 +0200

    After database configuration

--- src/main/java/be/icc/pid2526/reservations_spring/ReservationsSpringApplication.java
+++ src/main/java/be/icc/pid2526/reservations_spring/ReservationsSpringApplication.java
@@ -2,12 +2,18 @@ package be.icc.pid2526.reservations_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ReservationsSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationsSpringApplication.class, args);
 	}
-
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
 }
